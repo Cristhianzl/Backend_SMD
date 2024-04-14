22fa1a63ec9bebf6b6e0ca648592891cb5e5d4d8
cristhianzl <cristhian.lousa@gmail.com>
2024-04-14 11:44:18 -0300

    🔧 (main.ts): update CORS configuration to allow requests from specific origins and methods, and enable credentials support ✅ (main.ts): add comprehensive CORS configuration to improve security and allow cross-origin requests from specific origins and methods

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,17 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://frontend-smd.vercel.app',
+      'http://www.frontend-smd.vercel.app',
+      '*',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+    credentials: true,
+  });
+
   await app.listen(8080);
 }
 bootstrap();
